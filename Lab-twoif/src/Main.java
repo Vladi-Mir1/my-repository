42756f42d5b74e2a0162d1cb47f97414637cff8b
Vladi-Mir1 <vladimir@mironovich.com>
2014-04-05 18:10:04 +0400

    Main

--- Lab-twoif/src/Main.java
+++ Lab-twoif/src/Main.java
@@ -4,11 +4,16 @@ public class Main {
 	public static void main(String[] args) {
 		int x = 1;
 		while (x <= 100) {
-		if (x % 15 == 0){
-			System.out.println("foobar");
+			if (x % 15 == 0){
+				System.out.println("foobar");
+			} else if (x % 3 == 0) {
+				System.out.println("foo");
+			} else if (x % 5 == 0) {
+				System.out.println("bar");
+			} else {
+				System.out.println(x);
 			}
 			x++;
 		}
 	}
-
 }
