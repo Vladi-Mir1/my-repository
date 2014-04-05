3d14b3738947323fccfa0854911c6915405fb0c1
Vladi-Mir1 <vladimir@mironovich.com>
2014-04-05 18:03:17 +0400

    First commit!

---
+++ Lab-twoif/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x = 1;
+		while (x <= 100) {
+		if (x % 15 == 0){
+			System.out.println("foobar");
+			}
+			x++;
+		}
+	}
+
+}
